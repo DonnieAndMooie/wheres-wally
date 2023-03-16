13dfda06d201faf99bcdfe8488c79dd698bed119
DonnieAndMooie <owenjeaves@gmail.com>
2023-03-16 23:33:10 +0000

    Create and style header

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Wally from '../images/wally.png'
+
+export default function Header() {
+  return (
+    <div className='header'>
+        <img src={Wally} alt="Wally" class="header-wally" />
+        <h1>Where's Wally</h1>
+        <h2 className='timer'>0:00</h2>
+    </div>
+  )
+}
